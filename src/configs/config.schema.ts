abeabe7164aa786526d4faa263fd24a6217816db
Andrea Tombolato <andreacw96@gmail.com>
2022-09-20 21:53:40 +0200

    Remove default postgres user

--- src/configs/config.schema.ts
+++ src/configs/config.schema.ts
@@ -2,9 +2,9 @@ import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
   APP_PORT: Joi.number().default(3000).required(),
-  DATABASE_HOST: Joi.string().default('localhost').required(),
+  DATABASE_HOST: Joi.string().required(),
   DATABASE_PORT: Joi.number().default(5432).required(),
-  DATABASE_NAME: Joi.string().default('url-manager').required(),
-  DATABASE_USER: Joi.string().default('postgres').required(),
+  DATABASE_NAME: Joi.string().required(),
+  DATABASE_USER: Joi.string().required(),
   DATABASE_PASSWORD: Joi.string().default('').required(),
 });
