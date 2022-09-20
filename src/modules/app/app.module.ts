413a3d43bc5c7f612370ca6ab6830df40769528e
Andrea Tombolato <andreacw96@gmail.com>
2022-09-20 21:53:40 +0200

    Remove default postgres user

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -20,7 +20,7 @@ import { RolesGuard } from '../v1/roles/guards/roles.guard';
         type: 'postgres',
         host: configService.get('DATABASE_HOST'),
         port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USERNAME') || 'postgres',
+        username: configService.get('DATABASE_USERNAME'),
         password: configService.get('DATABASE_PASSWORD'),
         database: configService.get('DATABASE_NAME'),
         entities: ['dist/**/*.entity{.ts,.js}'],
