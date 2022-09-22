0adc1f9d8358a40c19469a01679229a1e5c9d95c
Andrea Tombolato <andreacw96@gmail.com>
2022-09-22 18:05:55 +0200

    Fix DATABASE_USERNAME Joi required env

--- src/configs/config.schema.ts
+++ src/configs/config.schema.ts
@@ -5,6 +5,6 @@ export const configValidationSchema = Joi.object({
   DATABASE_HOST: Joi.string().required(),
   DATABASE_PORT: Joi.number().default(5432).required(),
   DATABASE_NAME: Joi.string().required(),
-  DATABASE_USER: Joi.string().required(),
+  DATABASE_USERNAME: Joi.string().required(),
   DATABASE_PASSWORD: Joi.string().default('').required(),
 });
